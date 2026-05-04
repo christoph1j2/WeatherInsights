f188266111486a49afb54351a1003b647619c7d1
christoph1j2 <ernstleska@gmail.com>
2026-05-04 09:07:36 +0200

    Add project files.

---
+++ Models/GeocodingResult.cs
@@ -0,0 +1,23 @@
+﻿namespace LeuzeWeather.Models
+{
+    /// <summary>
+    /// Model class for the Geocoding API result from 
+    /// Open-Meteo.
+    /// </summary>
+    public class GeocodingResult
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+    }
+
+    /// <summary>
+    /// Holds a list of all the results it fetches from the API.
+    /// </summary>
+    public class GeocodingResultWrapper
+    {
+        public List<GeocodingResult> results { get; set; }
+    }
+}
